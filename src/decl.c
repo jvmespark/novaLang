13763910fc85d4b3e1f20218a49fc3d763b05a70
jvmespark <jamesdpark1@gmail.com>
2025-11-22 19:48:33 -0500

    function ast node

--- src/decl.c
+++ src/decl.c
@@ -9,3 +9,15 @@ void var_declaration() {
     genglobsym(Text);
     semi();
 }
+
+struct ASTnode *function_declaration(void) {
+    struct ASTnode *tree;
+    int nameslot;
+    match(T_VOID, "void");
+    ident();
+    nameslot = addglob(Text);
+    lparen();
+    rparen();
+    tree = compound_statement();
+    return(mkastunary(A_FUNCTION, tree, nameslot));
+}
